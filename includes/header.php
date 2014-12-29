9ad6035e6d7cb60bd94df5e9cb80d146c077c8b3
JackWong025 <617438040@qq.com>
2014-12-30 00:17:34 +0800

    move the login and sing up dis to header.php

--- includes/header.php
+++ includes/header.php
@@ -42,4 +42,70 @@
             <div class="white_bar"></div>
 
         </div>
-        <!-- 头部导航栏 -->
+<!-- 头部导航栏 -->
+<!-- 提示登录注册 -->
+        <div class="tips_reg_log_con box_con">
+            <div class="tips_main">
+                <a class="tips_join">成为CdM居民，体验分享的快乐</a>
+                <div class="tips_btns">
+                    <a class="tips_reg_btn">现在注册</a>
+                    <a class="tips_log_btn">问友登录</a>
+                </div>
+            </div>
+        </div>
+
+    <!-- 提问窗口 -->
+    <div id="dia_ask">
+        <form class="dia_ask_form">
+            <p>
+                <lable for="ask_title">标 题 ：</lable>
+                <input type="text" id="ask_title" name="ask_title" class="round_textbox" placeholder="请输入标题" required>
+            </p>
+            <lable>描 述 ：</lable>
+            <div class="details_textarea_div">
+                <textarea id="ask_details" name="ask_details" class="round_textbox"></textarea>
+            </div>
+            <input type="submit" class="dia_submit" value="提交">
+        </form>
+    </div>
+
+    <!-- 登录对话窗口 -->
+    <div id="dia_login" title="问友登录">
+        <form class="dia_login_form">
+            <p>
+                <lable for="login_email">账 号 ：</lable>
+                <input type="text" id="login_email" name="login_email" class="round_textbox dia_textbox" placeholder="请输入注册邮箱" required>
+            </p>
+            <p>
+                <lable for="login_pass">密 码 ：</lable>
+                <input type="password" id="login_pass" name="login_pass" class="round_textbox dia_textbox" placeholder="请输入密码" required>
+            </p>
+            <p>
+                <input type="checkbox" name="login_save_cookie" id="login_save_cookie" checked/>
+                <label for="login_save_cookie">一个月内自动登录</label>
+            </p>
+            <input type="submit" class="dia_submit" value="登录">
+        </form>
+    </div>
+
+    <!-- 注册对话窗口 -->
+    <div id="dia_reg">
+        <form class="dia_reg_form">
+            <p>
+                <lable for="reg_email">邮 箱 ：</lable>
+                <input type="text" id="reg_email" name="reg_email" class="round_textbox dia_textbox" placeholder="将作为账号以及安全邮箱">
+            </p>
+            <p>
+                <lable for="reg_user">昵 称 ：</lable>
+                <input type="text" id="reg_user" name="reg_user" class="round_textbox dia_textbox" placeholder="请输入您的昵称">
+            </p>
+            <p>
+                <lable for="reg_pass">密 码 ：</lable>
+                <input type="password" id="reg_pass" name="reg_pass" class="round_textbox dia_textbox" placeholder="密码至少6位">
+            </p>
+            <input type="submit" class="dia_submit" value="加入我们">
+        </form>
+    </div>
+
+    <!-- loading窗口 -->
+    <div id="dia_load">请稍候...</div>
